d14f20881bbc96d6ab12f26d12173461533cf684
Bainbridge <jack.bainbridge@medavie.bluecross.ca>
2022-10-25 11:50:23 -0300

    Adding basic query methods to retrieve data from Spring JPA Repository

---
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -0,0 +1,35 @@
+package guru.springframework.controllers;
+
+import guru.springframework.model.Category;
+import guru.springframework.model.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Controller
+public class IndexController {
+
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+
+    @RequestMapping({"","/","/index"})
+    public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByCategoryDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByUnitOfMeasure("Teaspoon");
+
+        System.out.println("Category ID: " + categoryOptional.get().getId());
+        System.out.println("UnitOfMeasure ID: " + unitOfMeasureOptional.get().getId());
+
+        return "index";
+    }
+}
